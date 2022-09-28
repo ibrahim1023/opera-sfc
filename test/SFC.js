555e8d4fb60aee7f073a3b27a74b8cf21f25b285
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2022-09-28 17:20:40 +0400

    Add conditions to allow only valid pubkey

--- test/SFC.js
+++ test/SFC.js
@@ -115,7 +115,9 @@ class BlockchainNode {
     }
 }
 
-const pubkey = '0x00a2941866e485442aa6b17d67d77f8a6c4580bb556894cc1618473eff1e18203d8cce50b563cf4c75e408886079b8f067069442ed52e2ac9e556baa3f8fcc525f';
+const pubkey = '0xc004ad15bf79efee161507f23df3d571021d08a1ac3cc14beb4a9a204f0c60487298d1d736b9fc6f53779c9579968a9421f411d60728d9dac85ad1286c1ca0e82d8a';
+const invalidPubKey = '0x00a2941866e485442aa6b17d67d77f8a6c4580bb556894cc1618473eff1e18203d8cce50b563cf4c75e408886079b8f067069442ed52e2ac9e556baa3f8fcc5';
+const zeroPubKey = '0xc004ad15bf79ef5d7cbdb0f629a6fd7a27b4597fcbf9b7bd9b764efef4ba72b3d4890c89e677a69ffd6f8160c7f0da8b000000000000000000000000000000000000';
 
 contract('SFC', async ([account1, account2]) => {
     let nodeIRaw;
@@ -303,7 +305,21 @@ contract('SFC', async ([firstValidator, secondValidator, thirdValidator]) => {
                 await expectRevert(this.sfc.createValidator(web3.utils.stringToHex(''), {
                     from: secondValidator,
                     value: amount18('10'),
-                }), 'empty pubkey');
+                }), 'invalid pubkey');
+            });
+
+            it('Should fail if pubkey is invalid (shorter)', async () => {
+                await expectRevert(this.sfc.createValidator(invalidPubKey, {
+                    from: secondValidator,
+                    value: amount18('10'),
+                }), 'invalid pubkey');
+            });
+
+            it('Should fail if last bytes of pubkey contains 0', async () => {
+                await expectRevert(this.sfc.createValidator(zeroPubKey, {
+                    from: secondValidator,
+                    value: amount18('10'),
+                }), 'invalid pubkey');
             });
 
             it('Should create two Validators and return the correct last validator ID', async () => {
